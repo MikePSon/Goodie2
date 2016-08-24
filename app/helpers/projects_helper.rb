3a04e4617015dc0e5889eb6422c5dd7503d7be07
Mike Patterson <info@mikeypatterson.com>
2016-08-24 11:15:54 -0400

    Project and Cycle Forms

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,6 @@
 module ProjectsHelper
+  def mission_helper
+  	raw('<i style="position:relative;top:1px; margin-left:5px" class="icon-question" data-container="body" data-toggle="popover" data-placement="right" data-title="Project Mission?" data-content="Two to three sentences about the goal of this project."> </i>')
+  end
+
 end
